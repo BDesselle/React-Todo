3ccdf61d0a571f659da385bb4d8c6c492b59e1fd
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-06 15:46:05 -0500

    Update TodoList.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,22 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = props => {
+  return (
+    <div>
+      {props.todos.map((
+        todo // Mapping out the todos into individual Todo components
+      ) => (
+        <Todo
+          handleToggleComplete={props.handleToggleComplete} // handleToggleComplete runs event passed via props
+          key={todo.id}
+          todo={todo}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
